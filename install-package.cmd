536bbf4ec513fe0b6a1449928d0b7b5d4c2cfcf3
Dirk Arnez <smalldirkalex@gmail.com>
2023-03-17 23:17:00 +0800

    - update install-package.cmd

--- install-package.cmd
+++ install-package.cmd
@@ -1,5 +1,5 @@
 @echo off
-set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.7.9-amd64\
+set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable\
 set PATH=%PYTHON_DIR%;%PYTHON_DIR%Scripts
 
 python -m pip install --upgrade pip
