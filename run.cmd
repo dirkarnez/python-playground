113432858435851be1eb1a5582c1b5ad228ce8cd
Dirk Arnez <smalldirkalex@gmail.com>
2023-07-27 18:40:06 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
 @echo off
-set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable\
-set PATH=%PYTHON_DIR%;%PYTHON_DIR%Scripts
+set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable
+set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
 
 python main.py
 
