c9db98106f9cf63e7ff7b72042bf161cd06042a9
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-21 17:48:41 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -8,14 +8,14 @@ set PREFIX=D:\Softwares
 
 
 set SEVENZIP=C:\"Program Files"\7-Zip\7z.exe
-set PYTHON_DIR=%PREFIX%\python-3.10.8-amd64-portable
+set PYTHON_DIR=%PREFIX%\python-3.13.9-amd64-portable
 set PYTHON_EXE=%PYTHON_DIR%\python.exe
 
 if not exist %PYTHON_EXE% (
 cd /d "%TEMP%" &&^
-%SystemRoot%\System32\curl.exe "https://github.com/dirkarnez/python-portable/releases/download/v3.10.8/python-3.10.8-amd64-portable.zip" -L -O  &&^
-%SEVENZIP% x python-3.10.8-amd64-portable.zip -o"%PYTHON_DIR%"  &&^
-del python-3.10.8-amd64-portable.zip
+%SystemRoot%\System32\curl.exe "https://github.com/dirkarnez/python-portable/releases/download/v3.13.9/python-3.13.9-amd64-portable.zip" -L -O  &&^
+%SEVENZIP% x python-3.13.9-amd64-portable.zip -o"%PYTHON_DIR%"  &&^
+del python-3.13.9-amd64-portable.zip
 )
 
 if exist %PYTHON_EXE% (
